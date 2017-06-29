767f112f19b2ab4d7278e1230e36748270e928f1
cy2003 <catyyee@hotmail.com>
2017-06-29 15:59:46 -0400

    Working search function

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,17 @@
+require 'pry'
+
 class StaticPagesController < ApplicationController
 
   def root
+    if !!params["query"]
+      @rubygems = Gems.search params["query"]
+      @rubygems.each do |i|
+        if i["name"] == params["query"]
+          @rubygem = i
+        end
+      end
+    end
+    render :root
   end
 
   def favorites
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root to: "static_pages#root"
+  post '/' => "static_pages#root", as: :search
   get '/favorites' => "static_pages#favorites"
+
 end
