1cfe21fca5ccc51831b5b56a5ec288d4a8dae851
cy2003 <catyyee@hotmail.com>
2017-07-05 17:28:25 -0400

    Cleaned up the code

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,54 +20,30 @@ $(document).ready(function(){
 
   var favoriteGems = JSON.parse(localStorage.getItem("rubygems")) || []
 
-  function changeStar(){
-    if($(this).attr("src") == "/assets/star-gray.png"){
-      $(this).attr("src", "/assets/star-blue.png")
-      favoriteGems.push($(this).closest("div").first().text().replace(/\s+/g, ''))
-      localStorage.setItem("rubygems", JSON.stringify(favoriteGems))
-    }
-    else{
-      $(this).attr("src", "/assets/star-gray.png")
-      favoriteGems = favoriteGems.filter(item => item !== $(this).closest("div").first().text().replace(/\s+/g, ''))
-      localStorage.setItem("rubygems", JSON.stringify(favoriteGems))
-    }
-  }
-
-  $(".star-pic").on("click", changeStar)
-
-  // var arrayGems = localStorage.getItem("rubygems")
-  // JQuery.ajax()
-
   function checkFavorites(){
     var storage = JSON.parse(localStorage.getItem("rubygems"))
     for(var i = 0; i < storage.length; i++){
       if($("div").hasClass(storage[i])){
-        $("." + storage[i]).replaceWith(`<a target="_blank" href="https://rubygems.org/gems/${storage[i]}">${storage[i]}</a><img class="star-pic" src="/assets/star-blue.png" alt="star pic"/>`)
+        $("." + storage[i]).replaceWith('<div class="' +storage[i]+ '"><a target="_blank" href="https://rubygems.org/gems/'+storage[i]+'">' +storage[i]+ '</a><img class="star-pic" src="/assets/star-blue.png" alt="star pic"/></div>')
       }
     }
   }
 
   checkFavorites()
 
+    function changeStar(){
+      if($(this).attr("src") == "/assets/star-gray.png"){
+        $(this).attr("src", "/assets/star-blue.png")
+        favoriteGems.push($(this).closest("div").first().text().replace(/\s+/g, ''))
+        localStorage.setItem("rubygems", JSON.stringify(favoriteGems))
+      }
+      else if ($(this).attr("src") == "/assets/star-blue.png"){
+        $(this).attr("src", "/assets/star-gray.png")
+        favoriteGems = favoriteGems.filter(item => item !== $(this).closest("div").first().text().replace(/\s+/g, ''))
+        localStorage.setItem("rubygems", JSON.stringify(favoriteGems))
+      }
+    }
 
-  // function checkFavorites(){
-  //   var storage = JSON.parse(localStorage.getItem("rubygems"))
-  //   var gems = []
-  //   $(".ruby-gem a").map(function(){
-  //     gems.push($(this).text())
-  //   })
-  //   for (var i = 0; i < gems.length; i++){
-  //     if(storage.includes(gems[i])){
-  //       var gemFavorite = gems[i]
-  //       var findDiv = $("a:contains("+ gemFavorite + ")")
-  //       var findSrc = findDiv.next().attr("src")
-  //       var findImgClass = findDiv.next().attr("class")
-  //       if(findSrc == "/assets/star-gray.png"){
-  //         debugger
-  //         $("." + gemFavorite).attr("src", "/assets/star-blue.png")
-  //       }
-  //     }
-  //   }
-  // }
+    $(".star-pic").on("click", changeStar)
 
 })
